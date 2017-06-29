453a2c767c1a8c2d8008a4497993321b9acd9557
lizziekarr <lizziemoves@gmail.com>
2017-06-29 12:31:35 -0500

    redesign

--- PlayController.js
+++ PlayController.js
@@ -1,17 +1,29 @@
 (function(){
-	'use strict'
 
+	'use strict'
 angular
 	.module('app')
 	.controller('PlayController', PlayController);
 
 PlayController.$inject = ['$scope', '$http'];
 	function PlayController($scope, $http) {
-		console.log('hi2')
-		
-		$scope.playerShowing = true;
+		alert('play')
+		$scope.showNav = false;
+		$scope.toggleNav = function(){
+			if($scope.showNav === false){
+				$(scope.showNav = true;)
+			}
+			else {
+				$scope.showNav = false;
+			}
+		}
+		$scope.playerShowing = false;
 		$scope.play = function(){
 			$scope.playerShowing = true;
+			$('nav').fadeOut('slow');
+		};
+		$scope.exit = function(){
+			$scope.playerShowing = false;
 		};
 	}
 })();
--- app.js
+++ app.js
@@ -1,29 +1,65 @@
 (function(){
-'use strict';
-angular
-	.module('app', ['ngRoute', 'ngCookies'])
-	.config(config)
-	.run(run);
 
-config.$inject = ['$routeProvider', '$locationProvider'];
-function config($routeProvider, $locationProvider) {
-		console.log('hi1')
-		$routeProvider
-				.when('/', {
-						controller: 'PlayController',
-						templateUrl: './index.html',
-						controllerAs: 'PlayController'
-				})
-				.when('/profile', {
-						controller: 'ProfileController',
-						templateUrl: './profile/index.html',
-						controllerAs: 'ProfileController'					
-				})
-}
+	$(document).ready(function(){
 
-run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
-function run($rootScope, $location, $cookieStore, $http) {
-		// keep user logged in after page refresh
-		$rootScope.globals = $cookieStore.get('globals') || {};
-}
+//play videos
+		$('.left a').on('click', function(){
+			$('iframe').hide();
+			$('.play .fa').show();
+		});
+
+		$('.a').on('click', function(){
+			$('.play .fa').hide();
+			$('iframe').hide();
+			$('.number').fadeIn();
+		});
+
+		$('.b').on('click', function(){
+			$('.play .fa').hide();
+			$('iframe').hide();
+			$('.asphyxiate').fadeIn();
+		});
+
+		$('.c').on('click', function(){
+			$('.play .fa').hide();
+			$('iframe').hide();
+			$('.lemon').fadeIn();
+		});
+
+		$('.d').on('click', function(){
+			$('.play .fa').hide();
+			$('iframe').hide();
+			$('.milkshake').fadeIn();
+		});
+
+		$('.e').on('click', function(){
+			$('.play .fa').hide();
+			$('iframe').hide();
+			$('.bad').fadeIn();
+		});
+
+		$('.f').on('click', function(){
+			$('.play .fa').hide();
+			$('iframe').hide();
+			$('.work').fadeIn();
+		});
+
+		$('.g').on('click', function(){
+			$('.play .fa').hide();
+			$('iframe').hide();
+			$('.pop').fadeIn();
+		});
+
+		$('.play .fa').on('click', function(){
+			$('.play .fa').hide();
+			$('iframe').hide();
+			$('.asphyxiate').fadeIn();
+		});
+
+//show Contact
+	$('.contact-link').on('click', function(){
+		$('.zero').show();
+	});
+
+	});
 })();
